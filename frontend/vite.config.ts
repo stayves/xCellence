0a659326bd3bb7727b6e990e6da257cf5335ec5c
stayves <atogambaev@gmail.com>
2025-11-16 02:25:01 +0500

    o

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // base: '/xCellence/',
-  plugins: [react()],
   base: '/xCellence/',
+  plugins: [react()],
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
 })
